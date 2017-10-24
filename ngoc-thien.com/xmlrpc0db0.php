1df5f65466f765d1b2e9c398eed78082c9469452
tramnamhanhphuc <letuanbaoit123@yahoo.com>
2017-10-24 16:10:55 +0700

    web wedding

---
+++ ngoc-thien.com/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>https://ngoc-thien.com</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://ngoc-thien.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://ngoc-thien.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://ngoc-thien.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://ngoc-thien.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://ngoc-thien.com/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
